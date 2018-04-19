9dee251e59056d8408e3b3ed05342d0206a8f8a7
Cheng Zeng <zeng.chen@husky.neu.edu>
2018-04-19 00:50:15 -0400

    add disqus

--- assets/js/post.jsx
+++ assets/js/post.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import {Layout, Card} from "element-react";
 import ReactMarkdown from "react-markdown";
+import Disqus from "disqus-react";
 
 export default function ShowPost(props) {
     return <ShowPostClass id={props.id}/>;
@@ -13,6 +14,7 @@ class ShowPostClass extends React.Component {
         super(props);
 
         this.state = {
+            id: props.id,
             name: "",
             title: "",
             content: "",
@@ -46,23 +48,36 @@ class ShowPostClass extends React.Component {
     }
 
     render() {
+        const disqusShortname = 'movieforum';
+        const disqusConfig = {
+            url: "https://movie.czeng.me/post/"+this.state.id,
+            identifier: this.state.id,
+            title: this.state.title,
+        };
         return(
-            <Layout.Row type="flex" justify="center" align="top">
-                <Layout.Col span="12" xs="22">
-                    <Card header={
-                        <div style={{textAlign: "center"}}>
-                            <h3>
-                                {this.state.title}
-                            </h3>
-                        </div>
-                    }>
-                        <ReactMarkdown source={this.state.content}
-                                    escapeHtml={false}
-                                    className="markdown-body"
-                        />
-                    </Card>
-                </Layout.Col>
-            </Layout.Row>
+            <div>
+                <Layout.Row type="flex" justify="center" align="top">
+                    <Layout.Col span="12" xs="22">
+                        <Card header={
+                            <div style={{textAlign: "center"}}>
+                                <h3>
+                                    {this.state.title}
+                                </h3>
+                            </div>
+                        }>
+                            <ReactMarkdown source={this.state.content}
+                                        escapeHtml={false}
+                                        className="markdown-body"
+                            />
+                        </Card>
+                    </Layout.Col>
+                </Layout.Row>
+                <Layout.Row type="flex" justify="center" align="top">
+                    <Layout.Col span="12" xs="22">
+                        <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig}/>
+                    </Layout.Col>
+                </Layout.Row>
+            </div>
         );
     }
 }
