8593c774575e61bfab2a2dc0161f2ee98823d533
Cheng Zeng <zeng.chen@husky.neu.edu>
2018-04-19 01:39:10 -0400

    i18n

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -13,7 +13,7 @@ export default function Index() {
     return (
         <div>
             <Layout.Row type="flex" justify="center">
-                <Layout.Col span="10" xs="24" sm="16" lg="12">
+                <Layout.Col span="10" xs="24" sm="16" lg="10">
                     <PosterCarousel/>
                 </Layout.Col>
             </Layout.Row>
